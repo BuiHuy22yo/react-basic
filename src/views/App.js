5239621ac4ec4ec6cb183932f53e5aebec3c4ce7
huybv@aht-asia.com <Huy@1998>
2021-12-21 23:34:06 +0700

    react router

--- src/views/App.js
+++ src/views/App.js
@@ -1,45 +1,58 @@
 import logo from './logo.svg';
 import './App.scss';
-import { MyComponent, MyComponent2 } from "./example/MyComponent";
+import {MyComponent, MyComponent2} from "./example/MyComponent";
 import {Form} from "./example/Form";
 import ListTodo from "./Todos/ListTodo";
-import { ToastContainer, toast } from 'react-toastify';
+import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/*<MyComponent></MyComponent>*/}
-        <ListTodo/>
-        {/*<a*/}
-        {/*  className="App-link"*/}
-        {/*  href="https://reactjs.org"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  Hoc react co ban*/}
-        {/*</a>*/}
-      </header>
 
-        <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-        />
-        {/* Same as */}
-        <ToastContainer />
-    </div>
-  );
+import Nav from "./Nav/Nav";
+import Home from "./Home/Home";
+import {
+    BrowserRouter ,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+function App() {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <header className="App-header">
+                    <Nav/>
+                    <img src={logo} className="App-logo" alt="logo"/>
+
+                    <Switch>
+                        <Route path="/" exact>
+                            <Home/>
+                        </Route>
+                        <Route path="/todo">
+                            <ListTodo/>
+                        </Route>
+                        <Route path="/about">
+                            <MyComponent/>
+                        </Route>
+                    </Switch>
+
+                </header>
+
+                <ToastContainer
+                    position="top-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
+                {/* Same as */}
+                <ToastContainer/>
+            </div>
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
